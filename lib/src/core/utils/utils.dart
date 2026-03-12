5fc3bb6318358f058dcee406723060d4483814ca
ahmedabufoda <ahmed.aboufoda@objects.email>
2026-03-12 14:58:50 +0200

    Add `flutter_svg` dependency and refactor `getImageFromRowData` to enable SVG image loading.

--- lib/src/core/utils/utils.dart
+++ lib/src/core/utils/utils.dart
@@ -2,6 +2,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Utils {
   static Future<Uint8List> getImageFromRowData({
@@ -9,23 +10,36 @@ class Utils {
     required double width,
     required double height,
   }) async {
-    var imageData = await rootBundle.load(image);
-    var imageCodec = await ui.instantiateImageCodec(
-      imageData.buffer.asUint8List(),
-      targetHeight: height.round(),
-      targetWidth: width.round(),
-    );
-    var imageFrameInfo = await imageCodec.getNextFrame();
-    var imageBytData = await imageFrameInfo.image.toByteData(
-      format: ui.ImageByteFormat.png,
-    );
-    return imageBytData!.buffer.asUint8List();
+    try {
+      final svgString = await rootBundle.loadString(image);
+
+      final pictureInfo = await vg.loadPicture(
+        SvgStringLoader(svgString),
+        null,
+      );
+
+      final picture = pictureInfo.picture;
+
+      final img = await picture.toImage(width.toInt(), height.toInt());
+
+      final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+
+      return byteData!.buffer.asUint8List();
+    } catch (e) {
+      throw Exception('Failed to convert SVG to image: $e');
+    }
   }
-  static Polyline displayPolyLine({required String polylineId,required List<LatLng>points,Color? color, int? width}){
+
+  static Polyline displayPolyLine({
+    required String polylineId,
+    required List<LatLng> points,
+    Color? color,
+    int? width,
+  }) {
     return Polyline(
-      polylineId:  PolylineId(polylineId),
+      polylineId: PolylineId(polylineId),
       points: points,
-      color: color??Colors.blue,
+      color: color ?? Colors.blue,
       width: width ?? 5,
     );
   }

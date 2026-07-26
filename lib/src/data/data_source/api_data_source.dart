5f846364e17d6bda395b667da9a693674266d345
Ahmed AbuFoda <ahmed.aboufoda@objects.email>
2026-07-26 12:12:50 +0300

    add geolocator and package_info_plus plugins, update place search request parameters, and remove minimum iOS version constraint

--- lib/src/data/data_source/api_data_source.dart
+++ lib/src/data/data_source/api_data_source.dart
@@ -2,6 +2,7 @@ import '../../core/network/api_constants.dart';
 import '../../core/network/dio_factory.dart';
 import '../models/places_autocomplete.dart';
 import '../models/places_details.dart';
+
 class ApiDataSource {
   final DioFactory dioFactory;
 
@@ -9,30 +10,38 @@ class ApiDataSource {
 
   Future<PlaceAutocompleteResponse> searchPlaces(String input) async {
     if (input.isEmpty) {
-      return  PlaceAutocompleteResponse(suggestions: []);
+      return PlaceAutocompleteResponse(suggestions: []);
     }
     try {
       final response = await dioFactory.post(
         ApiConstants.autocomplete,
-        data: {"input": input},
+        data: {
+          "input": input,
+          "includedPrimaryTypes": [
+            "street_address",
+            "premise",
+            "subpremise",
+            "route",
+          ],
+        },
       );
       if (response.data == null ||
           response.data is! Map<String, dynamic> ||
           response.data['suggestions'] == null) {
-        return  PlaceAutocompleteResponse(suggestions: []);
+        return PlaceAutocompleteResponse(suggestions: []);
       }
 
       return PlaceAutocompleteResponse.fromJson(response.data);
     } catch (e) {
       print("Place not found...$e");
-      return  PlaceAutocompleteResponse(suggestions: []);
-
+      return PlaceAutocompleteResponse(suggestions: []);
     }
   }
 
   Future<PlaceDetailsResponse> getPlaceDetails(String placeId) async {
-    final response = await dioFactory.get("${ApiConstants.placeDetails}$placeId");
+    final response = await dioFactory.get(
+      "${ApiConstants.placeDetails}$placeId",
+    );
     return PlaceDetailsResponse.fromJson(response.data);
-
   }
 }
